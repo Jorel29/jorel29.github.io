0e9f20f26e63acfacd7c4a5840a7bd03d51dcea8
Jorel Huerto <jjhue29@gmail.com>
2023-06-14 21:06:49 -0700

    drop down button testing begin

---
+++ astro-build/src/components/DropdownButton.svelte
@@ -0,0 +1,16 @@
+<script>
+    import HamburgerIcon from "./HamburgerIcon.svelte";
+</script>
+
+<style>
+    .dropdown-button{
+        cursor: pointer;
+        border-radius: 25px;
+        max-width: 75px;
+        border-color: transparent;
+    }
+</style>
+
+<button on:click class="dropdown-button">
+    <HamburgerIcon/>
+</button>
---
+++ astro-build/src/components/HamburgerIcon.svelte
@@ -0,0 +1,11 @@
+
+<svg class="hamburger-icon" viewBox="0 0 100 75" width="30" height="30">
+    <rect width="100" height="15" rx="10"></rect>
+    <rect y="30" width="100" height="15" rx="10"></rect>
+    <rect y="60" width="100" height="15" rx="10"></rect>
+</svg>
+<style>
+    .hamburger-icon{
+        fill: rgba(46, 1, 46, 1);
+    }
+</style>
