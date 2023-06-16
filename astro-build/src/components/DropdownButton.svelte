98e3cd1924ad3388787af200332faf21a06c055e
Jorel Huerto <jjhue29@gmail.com>
2023-06-16 16:36:20 -0700

    floating navbar

--- astro-build/src/components/DropdownButton.svelte
+++ astro-build/src/components/DropdownButton.svelte
@@ -1,13 +1,21 @@
 <script>
     import HamburgerIcon from "./HamburgerIcon.svelte";
+    
 </script>
 
 <style>
     .dropdown-button{
         cursor: pointer;
-        border-radius: 25px;
-        max-width: 75px;
+        border-radius: 50%;
+        width: 2rem;
+        height: 2rem;
         border-color: transparent;
+        align-items: center;
+        text-align: center;
+        background-color: darkslategray;
+    }
+    .dropdown-button:hover{
+        background-color: gray;
     }
 </style>
 
--- astro-build/src/components/HamburgerIcon.svelte
+++ astro-build/src/components/HamburgerIcon.svelte
@@ -1,11 +1,13 @@
 
-<svg class="hamburger-icon" viewBox="0 0 100 75" width="30" height="30">
-    <rect width="100" height="15" rx="10"></rect>
-    <rect y="30" width="100" height="15" rx="10"></rect>
-    <rect y="60" width="100" height="15" rx="10"></rect>
+<svg class="hamburger-icon" viewBox="0 0 100 100" width="100%" height="100%">
+    <rect y=0% width="100%" height="15%" rx="10%"></rect>
+    <rect y="33%" width="100%" height="15%" rx="10%"></rect>
+    <rect y="66%" width="100%" height="15%" rx="10%"></rect>
 </svg>
 <style>
     .hamburger-icon{
         fill: rgba(46, 1, 46, 1);
+        align-content: center;
     }
+    
 </style>
